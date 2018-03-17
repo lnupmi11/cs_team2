cf602276abd14c504572caed359bd404f2d8e6e4
andrewDubyk <andrew.dubyk7@gmail.com>
2018-03-17 17:16:16 +0300

    test for create method

--- xManikTest/ControllersTest/ServicesControllerTest.cs
+++ xManikTest/ControllersTest/ServicesControllerTest.cs
@@ -179,5 +179,20 @@ namespace xManikTest.ControllersTest
         }
 
         #endregion
+
+        #region CreateTests
+
+        [Fact]
+        public void Create_NoUser_ThrowApplicationException()
+        {
+            using (var context = new ApplicationDbContext(GetDbContextOptions()))
+            {
+                Service service = new Service() { Price = 10, Description = "test", Duration = 12 };
+                var controller = new ServicesController(context);
+                controller.Invoking(p => p.Create(service).Wait()).Should().Throw<ApplicationException>();
+            }
+        }
+
+        #endregion
     }
 }
